bd75cb1b11157352ae234e633a5d7cbaeb75fa27
paolo.bertin <paolo.bertin.62@gmail.com>
2022-01-03 03:32:23 +0100

    modifiche

--- src/main/java/eu/opensource/ordermanagement/web/controller/WelcomeController.java
+++ src/main/java/eu/opensource/ordermanagement/web/controller/WelcomeController.java
@@ -2,11 +2,13 @@ package eu.opensource.ordermanagement.web.controller;
 
 import eu.opensource.ordermanagement.domain.Cart;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/")
 @Controller
@@ -18,6 +20,8 @@ public class WelcomeController {
     public String homePage(Model uiModel) {
 
         uiModel.addAttribute("itemsInCart", cart.getNumberOfItems());
+
+        log.info("Benvenuti nel sito!");
 
         return "welcome";
     }
