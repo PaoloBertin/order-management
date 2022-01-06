979717de1b18036f63b3bfeb9106adf4cc8adcbf
paolo.bertin <paolo.bertin.62@gmail.com>
2022-01-06 16:42:58 +0100

    modifica configurazione cifratura password

--- src/main/java/eu/opensource/ordermanagement/config/PasswordEncoderConfiguration.java
+++ src/main/java/eu/opensource/ordermanagement/config/PasswordEncoderConfiguration.java
@@ -1,9 +1,18 @@
 package eu.opensource.ordermanagement.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @Configuration
 public class PasswordEncoderConfiguration {
@@ -11,9 +20,25 @@ public class PasswordEncoderConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
 
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 //        return new BCryptPasswordEncoder(4);
-//        return NoOpPasswordEncoder.getInstance();
-        return passwordEncoder;
+//      return NoOpPasswordEncoder.getInstance();
+
+//    return passwordEncoder;
+
+
+//    String idForEncode = "bcrypt";
+    String idForEncode = "noop";
+    Map<String,PasswordEncoder> encoders = new HashMap<>();
+    
+    encoders.put(idForEncode,   new BCryptPasswordEncoder());
+    encoders.put("noop",        NoOpPasswordEncoder.getInstance());
+//    encoders.put("pbkdf2",      new Pbkdf2PasswordEncoder());
+//    encoders.put("scrypt",      new SCryptPasswordEncoder());
+//    encoders.put("sha256",      new StandardPasswordEncoder());
+   
+    PasswordEncoder passwordEncoder = new DelegatingPasswordEncoder(idForEncode, encoders);
+
+    return passwordEncoder;
     }
 }
--- src/main/java/eu/opensource/ordermanagement/config/SecurityConfig.java
+++ src/main/java/eu/opensource/ordermanagement/config/SecurityConfig.java
@@ -22,54 +22,52 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(final AuthenticationManagerBuilder auth) throws Exception {
 
         auth.userDetailsService(userDetailsService)
-            .passwordEncoder(passwordEncoder)
-        ;
+                .passwordEncoder(passwordEncoder);
     }
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-//            .antMatchers("/**").permitAll()
-            .antMatchers("/resources/**").permitAll()
-            .antMatchers("/*").permitAll()
-            .antMatchers("/login/*").permitAll()
-            .antMatchers("/logout/*").permitAll()
-            .antMatchers("/errors").permitAll()
-            .antMatchers("/orders/**").hasAnyRole("USER", "ADMIN")
-            .antMatchers("/cart/**").hasAnyRole("USER", "ADMIN")
-            .antMatchers("/customers/*").hasAnyRole("USER", "ADMIN")
-            .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/**").permitAll()
+                .antMatchers("/resources/**").permitAll()
+                .antMatchers("/*").permitAll()
+                .antMatchers("/login/*").permitAll()
+                .antMatchers("/logout/*").permitAll()
+                .antMatchers("/errors").permitAll()
+                .antMatchers("/orders/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/cart/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/customers/*").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/admin/**").hasRole("ADMIN")
 
-            .and()
-            .formLogin()
-            .loginPage("/login/form")
-//            .loginProcessingUrl("/login")
-            .loginProcessingUrl("/authenticate")
-            .failureUrl("/login?error")
-            .defaultSuccessUrl("/default", true)
-            .permitAll()
-            .usernameParameter("username")
-            .passwordParameter("password")
+                .and()
+                .formLogin()
+                .loginPage("/login/form")
+                // .loginProcessingUrl("/login")
+                .loginProcessingUrl("/authenticate")
+                .failureUrl("/login?error")
+                .defaultSuccessUrl("/default", true)
+                .permitAll()
+                .usernameParameter("username")
+                .passwordParameter("password")
 
-            .and()
-            .logout()
-            .logoutUrl("/logout")
-            .logoutSuccessUrl("/login?logout")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
 
-            .and()
-            .httpBasic()
+                .and()
+                .httpBasic()
 
-            // CSRF is enabled by default, with Java Config
-            .and()
-            .csrf()
-            .disable()
+                // CSRF is enabled by default, with Java Config
+                .and()
+                .csrf()
+                .disable()
 
-            // Enable <frameset> in order to use H2 web console
-            .headers()
-            .frameOptions()
-            .disable()
-        ;
+                // Enable <frameset> in order to use H2 web console
+                .headers()
+                .frameOptions()
+                .disable();
 
     }
 }
