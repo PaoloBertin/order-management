154094754d1e6088209052217445e992e6078f88
paolo.bertin <paolo.bertin.62@gmail.com>
2022-01-10 15:56:47 +0100

    aggiunto commento

--- src/main/java/eu/opensource/ordermanagement/config/MvcConfiguration.java
+++ src/main/java/eu/opensource/ordermanagement/config/MvcConfiguration.java
@@ -10,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+/**
+ *
+ */
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
 
